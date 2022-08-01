54855dd324cb5319293d86cacd6d120bcc024fdd
tnty <97404151+tntyyy@users.noreply.github.com>
2022-08-01 16:51:19 +0300

    add: layout (#1)

---
+++ src/components/Temperature/Temperature.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Temperature.module.css';
+
+const Temperature = () => {
+  return <h1 className={styles.temperature}>9°</h1>;
+};
+
+export default Temperature;
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import Wrapper from '../Wrapper';
+import City from '../../components/City';
+import Temperature from '../../components/Temperature';
+import WeatherDescription from '../../components/WeatherDescription';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Wrapper>
+        <Temperature />
+        <WeatherDescription />
+        <City />
+      </Wrapper>
+    </div>
+  );
 };
 
 export default App;
