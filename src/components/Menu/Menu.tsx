1bb8cd491539b648860c273e59e1d8424d8c7486
tnty <tnty.working@gmail.com>
2022-08-02 12:34:40 +0300

    feat: swipes handling

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Menu.module.css';
+
+const Menu = () => {
+  return (
+    <div>Menu</div>
+  )
+}
+
+export default Menu
--- src/containers/Wrapper/Wrapper.tsx
+++ src/containers/Wrapper/Wrapper.tsx
@@ -10,6 +10,34 @@ interface IWrapper {
 
 const Wrapper: React.FC<IWrapper> = ({ children }) => {
   const [timeOfDay, setTimeOfDay] = useState<string | undefined>('');
+  const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false); 
+
+  let startX: number, startY: number, endingX: number, endingY: number;
+  let moving: boolean = false;
+
+  const handlerSwipeStart = (e: any) => {
+    startX = e.touches[0].clientX;
+    startY = e.touches[0].clientY;
+  }
+
+  const handlerSwipeMove = (e: any) => {
+    moving = true;
+    endingX = e.touches[0].clientX;
+    endingY = e.touches[0].clientY;
+  }
+
+  const handlerSwipeEnd = (e: any) => {
+    if (!moving) return;
+    let touchDirection;
+    if (!( Math.abs(endingX - startX) > Math.abs(endingY - startY))) {
+        if ( endingY > startY ) {
+          setIsOpenMenu(false);
+        } else {
+          setIsOpenMenu(true); //свапй открытия
+        }
+    }
+    moving = false;
+  }
 
   useEffect(() => {
     const time = getTimeOfDay(new Date().getHours());
@@ -20,8 +48,14 @@ const Wrapper: React.FC<IWrapper> = ({ children }) => {
     <main
       className={styles.wrapper}
       style={{ backgroundImage: `url(/static/${timeOfDay ? timeOfDay : 'noon'}.jpg)` }}
+      onTouchStart={(e) => handlerSwipeStart(e)}
+      onTouchMove={(e) => handlerSwipeMove(e)}
+      onTouchEnd={(e) => handlerSwipeEnd(e)}
     >
       {children}
+      {
+        isOpenMenu && <h1>Открыто</h1>
+      }
     </main>
   );
 };
